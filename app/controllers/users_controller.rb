c999c1b0a44edaa1c61ef0bbf59c34e5e6a5eba5
Cooliomoded <cooliomoded@gmail.com>
2020-10-19 16:30:36 -0700

    middle of html conversion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render 
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :skills
-  resources :fighters
-  resources :equipment
-  resources :squads
-  resources :users
+  resources :skills, only: [:index]
+  resources :fighters, only: [:new, :create, :edit, :update, :destroy]
+  resources :equipment, only: [:index]
+  resources :squads, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

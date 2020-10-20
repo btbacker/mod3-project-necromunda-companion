8ffe93ab0de4fbe542e7399930b15bf04ae79163
Cooliomoded <cooliomoded@gmail.com>
2020-10-20 16:37:32 -0700

    before my computer breaks

--- app/models/squad.rb
+++ app/models/squad.rb
@@ -2,4 +2,6 @@ class Squad < ApplicationRecord
   belongs_to :user
   has_many :fighters
   has_many :equipments
+
+  attribute :credits, :integer, default: 1000
 end
--- db/migrate/20201019175135_create_squads.rb
+++ db/migrate/20201019175135_create_squads.rb
@@ -2,6 +2,8 @@ class CreateSquads < ActiveRecord::Migration[6.0]
   def change
     create_table :squads do |t|
       t.string :name
+      t.string :house
+      t.integer :credits
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
